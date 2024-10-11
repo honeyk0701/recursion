e090d6e782b141c10745d172e6e0c842b13874c3
honeyk0701 <kashishsingh7504@gmail.com>
2024-10-11 11:34:06 +0530

    Update climbStairs.cpp

--- climbStairs.cpp
+++ climbStairs.cpp
@@ -1,1 +1,16 @@
-
+#include<iostream>
+using namespace std;
+int climbstairs(int n){
+    if(n<0)
+        return 0;
+    if(n==0)
+        return 1;
+    return climbstairs(n-1)+climbstairs(n-2);
+}
+int main(){
+    int n;
+    cin>>n;
+    int ans=climbstairs(n);
+    cout<<ans<<endl;
+    return 0;
+}
